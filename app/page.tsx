dec0d2b1b7d7648e7ebffafacd2d4460c80466b2
Shakil <sakildevmern@gmail.com>
2026-02-19 14:07:54 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+function page() {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia quas suscipit beatae laborum veniam nulla culpa cupiditate, accusantium earum nemo eius a optio ducimus! Esse sit, eaque totam corrupti maxime autem earum est et dignissimos rerum cupiditate incidunt vitae tempora quo nisi officia repellat tenetur aliquid. Dolor, temporibus. Incidunt optio voluptas ipsa magni, ut nostrum quia, cumque atque praesentium blanditiis ratione iusto quibusdam, animi aspernatur nesciunt recusandae quam quas. Eaque iure eligendi architecto beatae nostrum, sequi cumque nisi illo quo maiores velit minima placeat repellat et hic. Nesciunt doloribus sunt, molestiae velit voluptate unde consectetur veritatis perspiciatis laboriosam hic reprehenderit a at error quidem est aliquam itaque aliquid rem? Unde perspiciatis sint blanditiis ipsam, corrupti voluptas asperiores minima sapiente consequuntur non, quaerat recusandae saepe dolorum quo hic repudiandae magni possimus! Ipsa natus quam quos facilis pariatur corrupti tempora quasi amet, at delectus nesciunt voluptates, maxime tempore praesentium exercitationem cupiditate rerum iure, labore atque enim magnam in blanditiis. Delectus, et aliquid quaerat corrupti tempora autem eum omnis? Accusantium nisi fugiat ipsa dolorum modi ducimus necessitatibus doloribus inventore, sapiente impedit consectetur et est rem laboriosam laborum quia ad suscipit. Excepturi dignissimos facere hic ipsum at autem id atque unde vitae facilis. Consequatur.</h1>
+    </div>
+  )
+}
+
+export default page
