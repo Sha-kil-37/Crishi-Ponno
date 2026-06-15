9ecd69404eebc94d60a05a7e0628a695a2048953
Sha-kil-37 <sakildevmern@gmail.com>
2026-06-15 23:47:14 +0600

    last commit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,21 @@ export default function NotFound() {
   return (
     <section className="h-screen flex items-center justify-center">
       <div className="text-center">
-        <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
+        <h1 className="text-5xl font-bold text-red-500">Not Found</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          sequi quidem aperiam quibusdam suscipit architecto esse beatae totam,
+          nesciunt unde, debitis eveniet ducimus distinctio itaque veniam! Et
+          eaque unde reiciendis quasi dignissimos! Itaque dolorum maiores
+          doloremque veniam beatae mollitia assumenda pariatur placeat natus
+          praesentium ex sint obcaecati ut, nihil laborum voluptates expedita
+          vel consequuntur temporibus fugit explicabo dolor ea? Voluptatibus ut,
+          culpa aut quas magnam eaque facere quam dolorem possimus atque, dicta
+          qui consectetur pariatur quisquam dignissimos fugit laudantium vero
+          dolorum doloremque quasi quo blanditiis minima! Vitae commodi,
+          consequatur facilis saepe assumenda quibusdam laudantium magnam fugit
+          cumque? Quidem, temporibus voluptatum!
+        </p>
       </div>
     </section>
   );
