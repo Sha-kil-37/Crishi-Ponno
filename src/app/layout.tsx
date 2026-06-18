e9188a8f4b56f1ecd518517d53d977f411e2ecf3
Sha-kil-37 <sakildevmern@gmail.com>
2026-06-18 19:01:03 +0600

    6/18/2026

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
-import Navbar from "@/components/layout/navbar.tsx";
-import Footer from "@/components/layout/footer.tsx";
+import Navbar from "@/components/layout/navbar";
+import Footer from "@/components/layout/footer";
 //
 export const metadata: Metadata = {
   title: "Crishi Ponno",
